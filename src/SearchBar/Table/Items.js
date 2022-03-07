70ef85ab71730044ee4d108d76116516a001f998
Georgi Stoilkov <gogost6@gmail.com>
2022-03-07 23:08:15 +0200

    initial commit

---
+++ src/SearchBar/Table/Items.js
@@ -0,0 +1,20 @@
+const Items = ({ currentItems }) => {
+    return (
+        currentItems ? <>
+            {currentItems.map(x => <div className="symbol-container" key={x.symbol}>
+                <h4>{x.symbol}</h4>
+                <p>{x.lastPrice}</p>
+                <p
+                    style={x.priceChangePercent > 0
+                        ? { color: 'green' }
+                        : { color: 'red' }}>
+                    {x.priceChangePercent > 0
+                        ? `+${x.priceChangePercent}`
+                        : `${x.priceChangePercent}`}%</p>
+                <p>{Number(x.volume).toFixed(2)}M</p>
+            </div>)}
+        </> : ''
+    );
+}
+
+export default Items;
