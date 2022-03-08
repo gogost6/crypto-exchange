b93a37aa95c90bc61ccdf470decc46cd484238fa
Georgi Stoilkov <gogost6@gmail.com>
2022-03-08 17:06:59 +0200

    sorting added

--- src/SearchBar/Table/TableHeader.js
+++ src/SearchBar/Table/TableHeader.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { sortByName } from '../../features/exchange/exchangeSlice';
+import { sortByName, sortByPrice, sortBy24hrPriceChangePercent, sortByVolume } from '../../features/exchange/exchangeSlice';
 
 const TableHeader = () => {
     const sortType = useSelector(state => state.exchange.value.sortType);
@@ -13,30 +13,43 @@ const TableHeader = () => {
     const [hrChangeSort, setHrChangeSort] = useState(null);
     const [volumeSort, setVolumeSort] = useState(null);
 
-    const nameIcon = (sortType) => {
+    const fontIcon = (sortType) => {
         if (sortType == 'asc') {
-            return <FontAwesomeIcon icon={faSortUp} />
-        } else if (sortType == 'desc') {
             return <FontAwesomeIcon icon={faSortDown} />
+        } else if (sortType == 'desc') {
+            return <FontAwesomeIcon icon={faSortUp} />
+        } else {
+            return <FontAwesomeIcon icon={faSort} />
         }
     }
 
     const onClick = (e) => {
         const element = e.target;
-        
-        if (element.className == 'name' && sortType == 'asc') {
+        if (element.className == 'name' && (sortType.name == 'asc' || sortType.name == '')) {
             dispatch(sortByName('asc'));
-        } else if (element.className == 'name' && sortType == 'desc') {
+        } else if (element.className == 'name' && sortType.name == 'desc') {
             dispatch(sortByName('desc'));
+        } else if (element.className == 'price' && (sortType.price == 'asc' || sortType.price == '')) {
+            dispatch(sortByPrice('asc'));
+        } else if (element.className == 'price' && sortType.price == 'desc') {
+            dispatch(sortByPrice('desc'));
+        } else if (element.className == 'change' && (sortType['24hrPriceChangePercent'] == 'asc' || sortType['24hrPriceChangePercent'] == '')) {
+            dispatch(sortBy24hrPriceChangePercent('asc'));
+        } else if (element.className == 'change' && sortType['24hrPriceChangePercent'] == 'desc') {
+            dispatch(sortBy24hrPriceChangePercent('desc'));
+        } else if (element.className == 'volume' && (sortType.volume == 'asc' || sortType.volume == '')) {
+            dispatch(sortByVolume('asc'));
+        } else if (element.className == 'volume' && sortType.volume == 'desc') {
+            dispatch(sortByVolume('desc'));
         }
     }
 
     return (
         <div className="symbol-container small" onClick={onClick}>
-            <p className="name">Name {nameIcon(sortType)}</p>
-            <p className="price">Price <FontAwesomeIcon icon={faSort} /></p>
-            <p className="change">24h Change <FontAwesomeIcon icon={faSort} /></p>
-            <p className="volume">24h Volume <FontAwesomeIcon icon={faSort} /></p>
+            <p className="name">Name {fontIcon(sortType.name)}</p>
+            <p className="price">Price {fontIcon(sortType.price)}</p>
+            <p className="change">24h Change {fontIcon(sortType['24hr'])}</p>
+            <p className="volume">24h Volume {fontIcon(sortType.volume)}</p>
         </div>);
 }
 
--- src/features/exchange/exchangeSlice.js
+++ src/features/exchange/exchangeSlice.js
@@ -5,7 +5,12 @@ const initialState = {
     binanceAll: [],
     searchedPair: {},
     searchedPair24Hr: {},
-    sortType: ''
+    sortType: {
+      name: '',
+      price: '',
+      '24hrPriceChangePercent': '',
+      volume: ''
+    }
   },
 }
 
@@ -29,19 +34,70 @@ export const exchangeSlice = createSlice({
           const b = y.symbol
           return a.localeCompare(b);
         });
-        state.value.sortType = 'asc';
+        state.value.sortType.name = 'desc';
       } else if (action.payload == 'desc') {
         state.value.binanceAll.sort((x, y) => {
           const a = x.symbol;
           const b = y.symbol
           return b.localeCompare(a);
         });
-        state.value.sortType = 'desc';
+        state.value.sortType.name = 'asc';
+      }
+    },
+    sortByPrice: (state, action) => {
+      if (action.payload == 'asc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.lastPrice;
+          const b = y.lastPrice
+          return a - b;
+        });
+        state.value.sortType.price = 'desc';
+      } else if (action.payload == 'desc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.lastPrice;
+          const b = y.lastPrice;
+          return b - a;
+        });
+        state.value.sortType.price = 'asc';
+      }
+    },
+    sortBy24hrPriceChangePercent: (state, action) => {
+      if (action.payload == 'asc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.priceChangePercent;
+          const b = y.priceChangePercent;
+          return a - b;
+        });
+        state.value.sortType['24hrPriceChangePercent'] = 'desc';
+      } else if (action.payload == 'desc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.priceChangePercent;
+          const b = y.priceChangePercent;
+          return b - a;
+        });
+        state.value.sortType['24hrPriceChangePercent'] = 'asc';
+      }
+    },
+    sortByVolume: (state, action) => {
+      if (action.payload == 'asc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.volume;
+          const b = y.volume;
+          return a - b;
+        });
+        state.value.sortType.volume = 'desc';
+      } else if (action.payload == 'desc') {
+        state.value.binanceAll.sort((x, y) => {
+          const a = x.volume;
+          const b = y.volume;
+          return b - a;
+        });
+        state.value.sortType.volume = 'asc';
       }
     }
   },
 })
 
-export const { addPairs, addSearchedPair, add24Hr, sortByName } = exchangeSlice.actions;
+export const { addPairs, addSearchedPair, add24Hr, sortByName, sortByPrice, sortBy24hrPriceChangePercent, sortByVolume } = exchangeSlice.actions;
 
 export default exchangeSlice.reducer;
