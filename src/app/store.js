14542211117515845d550f42540d4fa883b5c1be
Georgi Stoilkov <gogost6@gmail.com>
2022-04-18 23:57:07 +0300

    git conflicts remove js files

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import exchangeReducer from '../features/exchange/exchangeSlice';
-
-export const store = configureStore({
-    reducer: {
-        exchange: exchangeReducer
-    },
-})
